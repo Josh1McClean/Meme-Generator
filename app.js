c8a3ce2665364ea19a66637fd607400e6d4ab5b5
thelateblo0mer <joshuamcclean1@gmail.com>
2023-10-10 21:16:44 -0400

    Final Adjustments!
    Had to restructure every file for the HTML, JavaScript, and CSS code
    blocks. In regards to the HTML there wasn't much to change however, I
    needed to add the "DOMContentLoaded" event listener as well as the
    remove function and id's/classes to call back in the JavaScript file.
    As for the CSS file, I added media queries for other devices and I
    to make an adjustment within my top and bottom classes to prevent the
    top and bottom texts from bunching together at the bottom of the image.

--- app.js
+++ app.js
@@ -17,39 +17,62 @@ darkModeBtn.addEventListener('click', function (e) {
     document.body.className = checked ? 'dark' : ''
 });
 
-//Selects the necessary id's for the form and input fields.
-let form = document.querySelector('#form');
-let urlInput = document.querySelector('#url');
-let topTextInput = document.querySelector('#top');
-let bottomTextInput = document.querySelector('#bottom');
-let memeContainer = document.querySelector('#meme');
+//Start of Meme Generator Components.
 
-//Attaches an event listener to the form.
-form.addEventListener('submit', function (e) {
-    e.preventDefault(); //Prevents the page from refreshing once we submit our information.
-    console.log('Form submitted');
+//This Event Listener Waits For The HTML Document To be Fully Loaded and Ready (i.e., When The DOM is Ready). Once The Document is Ready, The 
+//Function Inside The Event Listener is Executed. This is Used To Ensure That The JavaScript Code Runs After The HTML is Fully Loaded.
+document.addEventListener("DOMContentLoaded", function () {
+    let memeForm = document.getElementById("meme-form");
+    let listGallery = document.querySelector(".gallery");
 
-    //Grabs the values from their respective input fields.
-    let urlValue = urlInput.value;
-    let topTextValue = topTextInput.value;
-    let bottomTextValue = bottomTextInput.value;
+    //Allows The Web Page To Not Submit Upon Clicking The Submit Button.//Check Springboard Dark Mode vid for explanation on { checked }
+    memeForm.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-    //Creates a new div element for the meme
-    let memeDiv = document.createElement('div');
-    memeDiv.classList.add('meme');  //Adds this as a class for styling. 
+        //Creating The "li" Element
+        let memeLi = document.createElement("li");
+        memeLi.classList.add("meme-img");
 
-    //Sets the HTML content for the meme div using template literals.
-    memeDiv.innerHTML = `
-    <img src="${urlValue}" alt="Meme Image">
-    <div class="top-text">${topTextValue}</div>
-    <div class="bottom-text">${bottomTextValue}</div>
-    `;
+        //Creating The canvas Element To Put Image in Background
+        let urlInput = document.getElementById("url-images").value;
+        let src = urlInput;
+        let img = document.createElement("img");
+        img.src = src;
 
-    //Appends the meme div to the container.
-    memeContainer.appendChild(memeDiv);
+        let topTextDiv = document.createElement('div');
+        topTextDiv.classList.add("top", "text");
+        topTextDiv.innerHTML = document.getElementById("top-text").value;
 
-    //Clears the input fields for the next meme after they've been used.
-    urlInput.value = '';
-    topTextInput.value = '';
-    bottomTextInput.value = '';
-}); 
+        let bottomTextDiv = document.createElement("div");
+        bottomTextDiv.classList.add("bottom", "text");
+        bottomTextDiv.innerHTML = document.getElementById("bottom-text").value;
+
+        //Creating The "Remove" Element
+        let removeDiv = document.createElement("div");
+        removeDiv.classList.add("black-x");
+        removeDiv.innerText = "X";
+        removeDiv.style.color = "black";
+
+        //Takeing The Elements Needed To Represent A Meme & Appends Them To A Newly Created "li" Element (memeLi). "memeLi" is Then Appended To The 
+        //List (listGallery) To Display The Meme in The Web Page. 
+        listGallery.appendChild(memeLi);
+        memeLi.appendChild(img);
+        memeLi.appendChild(topTextDiv);
+        memeLi.appendChild(bottomTextDiv);
+        memeLi.appendChild(removeDiv);
+
+        //Creating The Reset To Prepare For The Addition of Another Meme.
+        memeForm.reset();
+    });
+
+    //The Remove Function is Used To Remove A Meme From The List When A User Clicks The "X" Button Next To That Meme. It Does This by Identifying 
+    //The Clicked "X" Button, Finding Its Parent (The "li" Element Representing The Meme), and Then Removing The Entire Meme (Both The Image and The
+    //Text) From The Web Page. This Function is Triggered by A Click Event on The "X" Button, Ensuring That The User Can Delete Memes From The List.
+    function remove(e) {
+        e.target.parentElement.remove();
+    }
+
+    //Settting Up an Event Listener To Respond To Click Events Within The "listGallery." When A User Clicks Within This List (For Example, on The 
+    //"X" Button of A Meme), The Remove Function is Executed, Allowing The User To Remove The Meme From The List.
+    listGallery.addEventListener("click", remove, false);
+});
